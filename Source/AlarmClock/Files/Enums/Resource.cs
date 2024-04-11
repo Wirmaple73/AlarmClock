b40f955841edd00415a9b0113f00cba68667c223
Wirmaple73 <wirmaple73@gmail.com>
2024-04-11 17:56:49 +0330

    Remove unused language entries

--- Source/AlarmClock/Files/Enums/Resource.cs
+++ Source/AlarmClock/Files/Enums/Resource.cs
@@ -6,7 +6,7 @@
 		MW_ListViewStatusEnabled, MW_ListViewStatusDisabled, MW_ListViewTime,
 		MW_ExitConfirmation, MW_UpToDate, MW_UpdateAvailable, MW_ConnectionError, MW_RegistryError,
 
-		ASW_NewAlarm, ASW_ModifyAlarm, ASW_AlarmNotSelected, ASW_AudioFileNotFound, ASW_AudioFileInvalid, ASW_SoundNotSelected,
+		ASW_NewAlarm, ASW_ModifyAlarm, ASW_AlarmNotSelected, ASW_SoundNotSelected,
 		ASW_OpenFileDialogTitle, ASW_OpenFileDialogFilter, ASW_SelectDaysToRepeat, ASW_TimeFormat, ASW_TimeNow,
 
 		ASW_Saturday, ASW_Sunday, ASW_Monday, ASW_Tuesday, ASW_Wednesday, ASW_Thursday, ASW_Friday, ASW_Once
